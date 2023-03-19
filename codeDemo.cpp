1c18c8e8984ef723326e4837273925bf4bdf4ea8
Hayat <hayatu4islam@yahoo.com>
2023-03-19 17:19:53 +0000

    input from user

--- codeDemo.cpp
+++ codeDemo.cpp
@@ -2,10 +2,15 @@
 // Exercise 01_03
 // Hello World, by Hayat Adeyemo
 #include <iostream>
+#include <string>
 
 using namespace std;
+using std::ofstream;
 
 int main(){
-    cout << "Hi There!" << endl;
+    string str;
+    cout << "Please enter your name: ";
+    cin >> str;
+    cout << "Hello, " << str << "!" << endl;
     return (0);
 }
