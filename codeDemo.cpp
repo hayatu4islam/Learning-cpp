cb96f52d14c727659462c08600c8d139bd9ce40d
Hayat <hayatu4islam@yahoo.com>
2023-03-19 16:59:01 +0000

    first c++ commit

---
+++ codeDemo.cpp
@@ -0,0 +1,11 @@
+// Learning C++
+// Exercise 01_03
+// Hello World, by Hayat Adeyemo
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hi There!" << endl;
+    return (0);
+}
